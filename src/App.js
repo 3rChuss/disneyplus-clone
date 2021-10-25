833930b2d09a657ee3ff10fe34ef996e961cc73e
3rChuss <3rchuss@gmail.com>
2021-10-25 17:09:16 +0200

    nailed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import {
   withRouter,
 } from "react-router-dom";
 import Hero from "./components/Hero";
-import LoginPage from "./components/views/Login";
+import LoginPage from "./components/pages/Login";
 import Header from "./components/Header";
 import "./App.css";
-import HomePage from "./components/views/Home";
-import Details from "./components/views/Details";
+import HomePage from "./components/pages/Home";
+import Details from "./components/pages/Details";
 
 function App(props) {
   return (
